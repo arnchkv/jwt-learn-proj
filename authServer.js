79349bfd362a5532880d21bf048d10ba25a9a358
Arnav <arnchkv@gmail.com>
2022-12-22 14:55:16 +0530

    formatted using vscode

--- authServer.js
+++ authServer.js
@@ -38,10 +38,10 @@ app.post('/login', (req, res) => {
 
 app.post('/token', (req, res) => {
 	const refreshToken = req.body.token
-	if(refreshToken == null) return res.sendStatus(401)
-	if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+	if (refreshToken == null) return res.sendStatus(401)
+	if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
 	jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-		if(err) return res.sendStatus(403)
+		if (err) return res.sendStatus(403)
 		const accessToken = generateAccessToken({ name: user.name })
 		res.json({ accessToken: accessToken })
 	})
