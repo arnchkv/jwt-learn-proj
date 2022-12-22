efcc17a3ddfc2f51588cb6005087aa636d0ae44d
Arnav <arnchkv@gmail.com>
2022-12-22 13:49:27 +0530

    refresh accesstoken created

--- authServer.js
+++ authServer.js
@@ -17,9 +17,7 @@ const posts = [
 	}
 ]
 
-app.get('/posts', authenticateToken, (req,res) => {
-	res.json(posts.filter(post => post.username === req.user.name))
-})
+let refreshTokens = []
 
 app.post('/login', (req, res) => {
 	// Authenticate User
@@ -27,21 +25,25 @@ app.post('/login', (req, res) => {
 	const username = req.body.username
 	const user = { name: username }
 
-	const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-	res.json({ accessToken: accessToken })
+	const accessToken = generateAccessToken(user)
+	const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+	refreshTokens.push(refreshToken)
+	res.json({ accessToken: accessToken, refreshToken: refreshToken })
 })
 
-function authenticateToken(req, res, next) {
-	const authHeader = req.headers['authorization']
-	const token = authHeader && authHeader.split(' ')[1]
-
-	if (token == null) return res.sendStatus(401)
-
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-		if (err) return res.sendStatus(403)
-		req.user = user
-		next()
+app.post('/token', (req, res) => {
+	const refreshToken = req.body.token
+	if(refreshToken == null) return res.sendStatus(401)
+	if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+	jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+		if(err) return res.sendStatus(403)
+		const accessToken = generateAccessToken({ name: user.name })
+		res.json({ accessToken: accessToken })
 	})
+})
+
+function generateAccessToken(user) {
+	return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' })
 }
 
 app.listen(3001)
